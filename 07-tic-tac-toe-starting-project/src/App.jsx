1c28221be32be13d526e7bbcd78b6937afd8877f
unknownMagician <vipnisyadav@gmail.com>
2024-09-10 01:20:47 +0530

    React Basic app

---
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -0,0 +1,47 @@
+import {useState} from 'react';
+import Player from './components/Player.jsx';
+import GameBoard from './components/GameBoard.jsx';
+import Log from './components/Log.jsx';
+import { WINNING_COMBINATIONS } from './winning-combinations.js';
+
+function deriveActivePlayer(gameTurns){
+  let currentPlayer ='X';
+
+  if(gameTurns.length >0 && gameTurns[0].player ==='X'){
+    currentPlayer = '0';
+  }
+  return currentPlayer;
+
+}
+function App() {
+  const[gameTurns,setGameTurns] = useState([]);
+  // const [activePlayer,setActivePlayer] = useState('X');
+  const activePlayer = deriveActivePlayer(gameTurns);
+  function handleSelectSquare(rowIndex,colIndex){
+    // setActivePlayer((curActivePlayer)=>curActivePlayer==='X'?'0':'X');
+    setGameTurns((prevTurns)=>{
+      const currentPlayer = deriveActivePlayer(prevTurns);
+      const updatedTurns = [{square:{row: rowIndex, col: colIndex}, player:currentPlayer},...prevTurns,
+      ];
+      return updatedTurns;
+    });
+  }
+  
+
+  return (
+    <main>
+      <div id = "game-container">
+      <ol id ="players" className='highlight-player'>
+        <Player initialName="Player 1" symbol="X" isActive={activePlayer==='X'}/>
+        <Player initialName="Player 2" symbol="0" isActive={activePlayer==='0'}/>
+      </ol>
+      <GameBoard onSelectSquare={handleSelectSquare} 
+      turns ={gameTurns}
+      />
+      </div>
+      <Log turns={gameTurns}/>
+    </main>
+  )
+}
+
+export default App
